616f0b342f68d0df164245edfffae990e5aeffcd
sarahBæk2021 <sbaek18@gmail.com>
2021-10-05 19:24:26 +0200

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,89 @@
+﻿using Book_Library;
+using Book_Rest_API.Managers;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Book_Rest_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private readonly BooksManager _manager = new BooksManager();
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{inISBN13}")]
+        public Book Get(string inISBN13)
+        {
+            return _manager.GetById(inISBN13);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public Book Post([FromBody] Book value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{inISBN13}")]
+        public Book Put(string inISBN13, [FromBody] Book value)
+        {
+            return _manager.Update(inISBN13, value);
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{inISBN13}")]
+        public Book Delete(string inISBN13)
+        {
+            return _manager.Delete(inISBN13);
+        }
+    }
+
+    //    // GET: api/<BooksController>
+    //    [HttpGet]
+    //    public IEnumerable<string> Get()
+    //    {
+    //        return new string[] { "value1", "value2" };
+    //    }
+
+    //    // GET api/<BooksController>/5
+    //    [HttpGet("{id}")]
+    //    public string Get(int id)
+    //    {
+    //        return "value";
+    //    }
+
+    //    // POST api/<BooksController>
+    //    [HttpPost]
+    //    public void Post([FromBody] string value)
+    //    {
+    //    }
+
+    //    // PUT api/<BooksController>/5
+    //    [HttpPut("{id}")]
+    //    public void Put(int id, [FromBody] string value)
+    //    {
+    //    }
+
+    //    // DELETE api/<BooksController>/5
+    //    [HttpDelete("{id}")]
+    //    public void Delete(int id)
+    //    {
+    //    }
+}
---
+++ Managers/BooksManager.cs
@@ -0,0 +1,65 @@
+﻿using Book_Library;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Book_Rest_API.Managers
+{
+    public class BooksManager
+    {
+        //private static string _nextISBN13;
+
+        private static readonly List<Book> Data = new List<Book>
+        {
+            new Book { ISBN13 = "978-0-000-00000-1", Title = "Beauty and the beast", Author = "Maria", PageNumber = 200 },
+            new Book{ISBN13 = "978-0-000-00000-2", Title = "Learning C#", Author = "Andreas", PageNumber = 200}
+        
+    };
+
+        public List<Book> GetAll()
+        {
+            return new List<Book>(Data);
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+        }
+
+        public Book GetById(string in_ISBN13)
+        {
+            return Data.Find(book => book.ISBN13 == in_ISBN13);
+        }
+
+        public Book Add(Book newBook)
+        {
+            //newBook.ISBN13 = _nextISBN13;//++ inakataa
+            Data.Add(newBook);
+            return newBook;
+        }
+
+
+        public Book Delete(string in_ISBN13)
+        {
+            Book book = Data.Find(book1 => book1.ISBN13 == in_ISBN13);
+            if (book == null) return null;
+            Data.Remove(book);
+            return book;
+        }
+        public Book Update(string in_ISBN13, Book updates)
+        {
+            Book book = Data.Find(book1 => book1.ISBN13 == in_ISBN13);
+            if (book == null) return null;
+            book.Title = updates.Title;
+            book.Author= updates.Author;
+            book.PageNumber = updates.PageNumber;
+            return book;
+        }
+
+    }
+
+
+
+
+}
+
